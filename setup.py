5894321ac9192832fa45a0f0503cc21324bcd8d3
Rodrigo F. Díaz <rodrigo.diaz@unige.ch>
2016-10-30 02:10:05 -0300

    Initial commit with basic structure.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pyboas',
+      description='Bayesian Observation predictor and Adaptive Scheduler.',
+      version='0.1.0',
+      author='Rodrigo F. Diaz',
+      author_email='rodrigo.diaz@unige.ch',
+      #url='http://obswww.unige.ch/~diazr/pygpr',
+      packages=['pyboas'],
+      requires=['numpy',]
+      )
